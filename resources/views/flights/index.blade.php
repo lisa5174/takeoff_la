a2e85135c3a89e82cd686f1ed574f514fbd56e11
lisa5174 <48669887+lisa5174@users.noreply.github.com>
2021-05-05 09:34:23 +0800

    今日航班頁面與前端結合

--- resources/views/flights/index.blade.php
+++ resources/views/flights/index.blade.php
@@ -1,19 +1,82 @@
 @extends('layouts.flights')
+{{-- 
+@section('p1.css')
 
+@endsection --}}
 
+@section('main')
+<div class="container ">
+    <div class=" row justify-content-start">
+      <div class="btn-group-vertical col-2" >
+          <aside >
+            <a class="dropdown-item " href="p1-today.html">今日航班</a>
+            <a class="dropdown-item" href="P2-on.html">上架</a>
+            <a class="dropdown-item" href="P3-out.html">下架</a>
+            <a class="dropdown-item" href="P4-fix.html">修改</a>
+          </aside>
+      </div> 
+      <span class="col-10" id="g1">
+        <nav class="nav nav-tabs ">    
+          <div  id="nav-tab " role="tablist" >
+            <a class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#" role="tab" aria-controls="nav-home" aria-selected="true">今日航班</a>
+         </div>
+        </nav>
+       <div class="tab-content" id="nav-tabContent">
+           <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+            <section class="table table-hover">
+            <div > <!--時間表-->
+              <table cellpadding="0" cellspacing="0" >
+                <thead>
+                  <tr class="tbl-header">
+                    <th><h6><b> 飛機名稱</th>
+                    <th><h6><b> 起飛時間</th>
+                    <th><h6><b> 起飛地點</th>
+                    <th><h6><b> 降落地點</th>
+                    <th><h6><b> 座位</th>
+                    <th><h6><b> 已售座位</th>
+                  </tr>
+                </thead>
+            </div>
+            <div class="tbl-content ">
+                <tbody>
+                    @foreach($flights as $flight)
+                        <tr>
+                            <td>{{ $flight->fName}}</td>
+                            <td>{{ $flight->time }}</td>
+                            <td>{{ $flight->toplace}}</td>
+                            <td>{{ $flight->foplace}}</td>
+                            <td>{{ $flight->airSeat}}</td>
+                            <td>{{ $flight->unboughtSeat}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+              </table>
+            </div>
+          </section>
+          <script>$(window).on("load resize ", function() {
+            var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+            $('.tbl-header').css({'padding-right':scrollWidth});
+          }).resize();</script>
+          </div>
+       </div>
+      </span>
+     </div> 
+    </div>
+@endsection
 
+{{-- 
 @section('main')
     <h1 class="font-thin text-4xl">今日航班</h1>
 
     @foreach($flights as $flight)
         <div>
             <h2 class="text-2xl">飛機名稱{{ $flight->fName}}</h2>
-            {{-- @php
+            @php
                 require 'vendor/autoload.php';
                 use Carbon\Carbon;
                 $tt = new Carbon($flight->time);
-            @endphp --}}
-            {{-- date('Y-m-d H:i:s', time()) --}}
+            @endphp
+            date('Y-m-d H:i:s', time())
             <p>起飛時間{{ $flight->time }}</p>
             <p>起飛地點{{ $flight->toplace}}</p>
             <p>降落地點{{ $flight->foplace}}</p>
@@ -23,6 +86,6 @@
     @endforeach
 
 
-{{-- {{$flights}} --}}
 
-@endsection
+
+@endsection --}}
--- resources/views/layouts/flights.blade.php
+++ resources/views/layouts/flights.blade.php
@@ -5,12 +5,73 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>後台</title>
+    <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="{{ asset('css/p1.css')}}"/>
+    <style>
+        /* @yield('p1.css') */
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Take off 空|後台_今日航班</title>
 </head>
 <body>
-<main class="m-4">
-    @yield('main')
-</main>    
+    <div>
+        <!-- Navigation導覽列 -->
+        <!-- navbar只有light,dark,為設定字體顏色 -->
+    <nav
+        class="navbar navbar-expand-lg navbar-dark static-top"
+        style="background-color: #6798c0">
+        <div class="container">
+            <a class="navbar-brand" href="homePage.html"> Take off 空 </a>
+            <div id="Date"> </div>
+        </a></div></a></div>
+
+        <div mv-app="clock" mv-bar="none">
+
+            <script type="text/javascript"> //現在時間
+            window.onload=function(){
+            setInterval(function(){
+            var date=new Date();
+            var year=date.getFullYear(); //獲取當前年份
+            var mon=date.getMonth()+1; //獲取當前月份
+            var da=date.getDate(); //獲取當前日
+            var day=date.getDay(); //獲取當前星期幾
+            var h=date.getHours(); //獲取小時
+            var m=date.getMinutes(); //獲取分鐘
+            var s=date.getSeconds(); //獲取秒
+            var d=document.getElementById('Date');
+            d.innerHTML='現在時間:'+year+'年'+mon+'月'+da+'日'+/*'星期'+day+*/' '+h+':'+m+':'+s; },1000) }</script>
+        </div>
+
+    <div>
+        @yield('main')
+    </div>
+
+
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script
+src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+crossorigin="anonymous"
+></script>
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+crossorigin="anonymous"
+></script>
+<script
+src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+crossorigin="anonymous"
+></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 </body>
 </html>
