05f664772aa40a4b0ffef2d779b808845a0cac23
gm3g0 <kittybtsv@gmail.com>
2021-06-10 09:51:08 +0800

    flights.blade的超連結還沒用好

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,1 +1,26 @@
 aflogin
+@extends('layouts.flights')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/b_today.css')}}"/>
+@endsection
+
+@section('title')
+    <title>Take off 空|後台_登入</title>
+@endsection
+
+@section('main')
+
+<div id="boxForm"> <!--登入-->
+    <h2 id="title">登入</h2>
+    <form>
+      <input class='text' type='user' name='user' placeholder='帳號' required>
+      <br>
+      <input class='text' id='pwd'  type='password' placeholder='密碼' required>
+      <br>
+      <input id='rememberMe' name='rememberMe' type='checkbox'> <label>記住我</label>
+      <br>
+      <input class='button' type='submit' value='登入'>
+    </form>
+  </div>
+@endsection
--- resources/views/updateticket/index.blade.php
+++ resources/views/updateticket/index.blade.php
@@ -1,1 +1,54 @@
-updateticket
+@extends('layouts.flights')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/b_today.css')}}"/>
+@endsection
+
+@section('title')
+    <title>Take off 空|後台_修改</title>
+@endsection
+
+@section('main')
+
+    @if (session()->has('notice')) 
+      <div class="m-2 bg-green-300 px-3 py-2 rounded">
+          {{ session()->get('notice')}}
+      </div>
+    @endif
+
+    @if ($errors->any())
+    <div class="errors m-2 p-1 bg-red-500 text-red-100 font-thin rounded">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif  
+
+<span class="col-10" id="g1"> <br>
+          <div class="container">
+            <div class="row"><h4>訂單資訊</h4></div><br>
+            <div class="row offset-md-1">
+              <div class="col-4">姓名：</div>
+              <div class="col-6">身分證字號：</div>
+            </div><br>
+            <div class="row offset-md-1">
+              <div class="col-4">性別：</div>
+              <div class="col-6">生日：</div>
+            </div><br>
+            <div class="row offset-md-1">
+              <div class="col-4">票種：</div>
+            </div><br>
+            <div class="row offset-md-1">
+              <div class="col-4">航班資訊：</div>
+            </div>
+          </div><br>
+          
+          <div class="d-grid gap-2 col-2 mx-auto">
+         <button type="submit" class="btn btn-primary">修改</button>
+      </div>
+
+
+
+@endsection
